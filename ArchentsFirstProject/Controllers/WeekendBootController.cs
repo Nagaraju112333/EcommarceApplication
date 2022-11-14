0ba8e9b6b289b4ffa260d846f2ab27a1b6f501a3
Nagaraju112333 <nagaraju.boda@archents.com>
2022-11-14 11:21:39 +0530

    first project

---
+++ ArchentsFirstProject/Controllers/WeekendBootController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArchentsFirstProject.Controllers
+{
+    public class WeekendBootController : Controller
+    {
+        // GET: WeekendBoot
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult WeekendBootMainPage()
+        {
+            return View();
+        }
+        public ActionResult Weekenboot()
+        {
+            return View();
+        }
+    }
+}
